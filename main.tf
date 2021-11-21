fa12936e664632a68696740e9f2769f87bc79760
Kevin Wood <kevin.wood75@gmail.com>
2021-11-21 14:44:20 -0500

    initial commit

--- main.tf
+++ main.tf
@@ -32,13 +32,12 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  site_config {
-    scm_type          = "ExternalGit"
-  }
- 
   source_control {
     repo_url           = "${var.giturl}"
     branch             = "${var.branch}"
   }
 
+  site_config {
+    scm_type          = "ExternalGit"
+  }
 }
