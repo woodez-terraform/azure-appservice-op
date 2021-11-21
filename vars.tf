49b193944e671c233268689c0c9afa05f5dd4031
Kevin Wood <kevin.wood75@gmail.com>
2021-11-21 13:13:35 -0500

    initial commit

--- vars.tf
+++ vars.tf
@@ -2,19 +2,28 @@ variable "location" {
   type    = string
   default = "eastus2"
 }
+
 variable "prefix" {
   type    = string
   default = "woodez"
 }
+
 variable  "giturl" {
   type    = string
 }
+
+variable "branch" {
+  type    = string  
+}
+
 variable "project" {
   type    = string
 }
+
 variable "tier" {
   type    = string  
 }
+
 variable "size" {
   type    = string  
 }
